c54d40e7f9bbe6131b3d01059db8dbbbc0f066f7
shahril <2675341+shahrilnet@users.noreply.github.com>
2025-05-30 09:42:32 +0800

    add 9.xx offsets

--- savedata/kernel_offset.lua
+++ savedata/kernel_offset.lua
@@ -195,6 +195,26 @@ ps5_kernel_offset_list = {
         PMAP_STORE_DMPDPI = nil,
     },
 
+    [{ "9.00", "9.05", "9.20", "9.40", "9.60" }] = {
+
+        DATA_BASE = 0xC90000,
+        DATA_SIZE = nil,
+
+        DATA_BASE_DYNAMIC = 0x10000,
+        DATA_BASE_TO_DYNAMIC = nil,
+        DATA_BASE_ALLPROC = 0x2765D50,
+        DATA_BASE_SECURITY_FLAGS = 0xD82064,
+        DATA_BASE_ROOTVNODE = 0x2FEB510,
+        DATA_BASE_KERNEL_PMAP_STORE = 0x2D38B78,
+        DATA_BASE_DATA_CAVE = nil,
+        DATA_BASE_GVMSPACE = 0x2D9A570,
+
+        PMAP_STORE_PML4PML4I = nil,
+        PMAP_STORE_DMPML4I = nil,
+        PMAP_STORE_DMPDPI = nil,
+
+    },
+
     [{ "10.00", "10.01" }] = {
 
         DATA_BASE = 0xCB0000,
